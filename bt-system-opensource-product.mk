20b2896aef54356904a46424ef1c211fa725eef3
Prateek Raj Singh <quic_pratsing@quicinc.com>
2025-04-23 14:28:26 +0530

    Revert "VSC: Enable BtVS System App support in build"
    This reverts commits - 0be86a6348d83255fd37bd3a58655ea3f9c66e88
    and 4c42e076d1aad7466b705d3b3c736cc1c8015f20
    
    CRs-Fixed: 4126800
    
    Change-Id: Ia749018b06238a4677415395f858803b25264edd

--- bt-system-opensource-product.mk
+++ bt-system-opensource-product.mk
@@ -38,7 +38,6 @@ PRODUCT_PACKAGES += Xpan
 PRODUCT_PACKAGES += privapp-permission-xpan.xml
 PRODUCT_PACKAGES += ChannelSoundingTestApp
 PRODUCT_PACKAGES += com.android.bluetooth.channelsoundingtestapp.xml
-PRODUCT_PACKAGES += BtVS
 endif #TARGET_HAS_LOW_RAM
 
 endif #TARGET_BOARD_TYPE
